993f78ddd8e2db2e08a04426d5c8d52169bb31f1
Himotlaan <himot.laan@tktk.ee>
2024-04-05 17:36:29 +0300

    add modal window

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,11 @@ const slides = document.getElementsByClassName('carousel__item');
 const slidesTotal = slides.length;
 const nextButton = document.querySelector('#carousel__button--next');
 const prevButton = document.querySelector('#carousel__button--prev');
+let moviePoster = document.querySelector('.movie-poster');
+let modal = document.querySelector('#myModal');
+let closeBtn = document.querySelector('.closeBtn');
+let modalImage = document.querySelector('.movie-poster-modal');
+
 
 let slidePosition = 0;
 
@@ -34,4 +39,20 @@ prevButton.addEventListener('click', () => {
     }
 
     slides[slidePosition].classList.add('carousel__item--visible');
+});
+
+moviePoster.addEventListener('click', ()=>{
+    console.log('image clicked');
+    modal.style.display = 'block';
+    modalImage.src = moviePoster.src;
+});
+
+closeBtn.addEventListener('click', ()=> {
+    modal.style.display = 'none';
+});
+
+window.addEventListener('click', (event)=> {
+    if(event.target === modal) {
+        modal.style.display = 'none';
+    }
 });
